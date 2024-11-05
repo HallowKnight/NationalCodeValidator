8ad3954127a067407aa74607efcc93dfb2475415
Amirmahdi Ghodsi <ghodsi.amirmahdi@yahoo.com>
2024-11-05 10:17:34 +0330

    Enabled IsPackable and packed

--- NationalCodeValidation/obj/Debug/net8.0/NationalCodeValidation.AssemblyInfo.cs
+++ NationalCodeValidation/obj/Debug/net8.0/NationalCodeValidation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NationalCodeValidation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91834e03a095a1820359ee81fda64901dc4945b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("NationalCodeValidation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NationalCodeValidation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
